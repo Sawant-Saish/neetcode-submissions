b52b075606f516562d3bed2fc5a5d72c8914269f
Saish Sawant <work.saishsawant@gmail.com>
2026-07-19 22:16:41 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        char[] arr = new char[n];
+        int j = -1;
+        
+        for (int i = 0; i < n; i++) {
+            char ele = s.charAt(i); 
+            
+            if (ele == '[' || ele == '(' || ele == '{') {
+                arr[++j] = ele;
+            } else if (ele == '}') {
+                if (j == -1 || arr[j] != '{') return false; 
+                j--;
+            } else if (ele == ')') {
+                if (j == -1 || arr[j] != '(') return false;
+                j--;
+            } else if (ele == ']') {
+                if (j == -1 || arr[j] != '[') return false;
+                j--;
+            }
+        }
+        return j == -1; 
+    }
+}
