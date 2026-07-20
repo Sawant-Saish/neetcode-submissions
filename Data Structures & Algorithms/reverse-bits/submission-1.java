9eab04d390420cfbb220698cbce1fd0d186c399b
Saish Sawant <work.saishsawant@gmail.com>
2026-07-20 10:32:11 +0530

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseBits(int n) {
+        int reversedN = 0;
+        for(int i = 0; i < 32; i++){
+            int bit = (n >> i) & 1;
+            if(bit == 1)
+            reversedN |= (1 << (31 - i));
+        }
+        return reversedN;
+    }
+}
